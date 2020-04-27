95f15bcf2549dc037aa91cdb797bfe373742731c
guoJohnny <401870532@qq.com>
2020-04-27 18:23:40 +0800

    serialize binary tree

--- src/main/scala/Tree/BinaryTree.scala
+++ src/main/scala/Tree/BinaryTree.scala
@@ -11,16 +11,17 @@ object BinaryTree {
     }
 
     /**
-    * Description：Given preorder and inorder traversal of a tree, construct the binary tree
-    * You may assume that duplicates do not exist in the tree.
-    * For example, given
-    * preorder = [3,9,20,15,7]
-    * inorder = [9,3,15,20,7]
-    * return [3,9,20,null,null,15,7]
-    * @param preorder   - a array of preorder traverse
-    * @param inorder   - a array of inorder traverse
-    * @return - root TreeNode
-    */
+     * 
+     * Description：Given preorder and inorder traversal of a tree, construct the binary tree
+     * You may assume that duplicates do not exist in the tree.
+     * For example, given
+     * preorder = [3,9,20,15,7]
+     * inorder = [9,3,15,20,7]
+     * return [3,9,20,null,null,15,7]
+     * @param preorder   - a array of preorder traverse
+     * @param inorder   - a array of inorder traverse
+     * @return - root TreeNode
+     */
     def preorderInorderHelper(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
         if (preorder.length == 0 || inorder.length == 0) return null
         var root = new TreeNode(preorder(0))
@@ -32,13 +33,19 @@ object BinaryTree {
                 inorder.slice(leftSubTreeNodeNums+1,inorder.length))          
         root
     }
-
+    
+    /**
+     * Recrusive serialize a binary tree from a root tree node
+     * @param root  - a root Treenode
+     * @param serial    - serialized string of tree
+     * @return - serialized string of tree
+     */
     def serialize(root: TreeNode, serial: String): String = {
         var tmp = serial
         if (root == null) {
-            tmp.concat("null,")
+            tmp = tmp + "null,"
         } else {
-            tmp.concat(root.value.toString.concat(","))
+            tmp = tmp + root.value.toString.concat(",")
             tmp = serialize(root.left, tmp)
             tmp = serialize(root.right, tmp)
         }
---
+++ src/test/scala/Tree/BinaryTreeSpec.scala
@@ -0,0 +1,11 @@
+package Tree
+import org.scalatest.FlatSpec
+
+class BinaryTreeSpec extends FlatSpec{
+  
+    "BinaryTree Serialize" should "return a serialized String passed to it" in {
+    val preOrder = Array(3,9,20,15,7)
+    val inOrder = Array(9,3,15,20,7)
+    assert(BinaryTree.serialize(BinaryTree.buildTree(preOrder,inOrder),"") === "3,9,null,null,20,15,null,null,7,null,null,")
+  }
+}
