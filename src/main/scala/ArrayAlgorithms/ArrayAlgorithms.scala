bb6101f9c5816d5ec66af875b4e9692e4e1432cb
guoJohnny <401870532@qq.com>
2020-06-11 08:29:00 +0800

    add findMedian

--- src/main/scala/ArrayAlgorithms/ArrayAlgorithms.scala
+++ src/main/scala/ArrayAlgorithms/ArrayAlgorithms.scala
@@ -1,6 +1,7 @@
 package ArrayAlgorithms
 
 import scala.collection.mutable.ArrayBuffer
+import scala.Array
 
 object ArrayAlgorithms {
     
@@ -16,5 +17,21 @@ object ArrayAlgorithms {
         }
         for (i <- 0 until index.length) array(i) = array(index(i))  
         array.trimEnd(array.length - index.length) 
+    }
+
+    /**
+      * Leetcode 3
+      * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+      * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+      * You may assume nums1 and nums2 cannot be both empty.
+      * @param nums1
+      * @param nums2
+      * @return
+      */
+    def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
+        val array = (nums1 ++ nums2).sorted
+        val mednum = array.length / 2
+        if (array.length % 2 == 0) return (array(mednum) + array(mednum - 1)) / 2.0
+        array(array.length/2)
     }
 }
--- src/test/scala/ArrayAlgorithms/ArrayAlgorithmsSpec.scala
+++ src/test/scala/ArrayAlgorithms/ArrayAlgorithmsSpec.scala
@@ -11,5 +11,10 @@ class ArrayAlgorithmsSpec extends FlatSpec{
         ArrayAlgorithms.removeNegativeNumber(arraybuffer)
         assert(arraybuffer.toArray === Array(2, 3, 11, -1, 22, 21))
     }
-
+    
+    "Median of Two Sorted Arrays" should "return a numbere passed to it" in {     
+        val nums1 = Array(1,3)
+        val nums2 = Array(2,4)
+        assert(ArrayAlgorithms.findMedianSortedArrays(nums1, nums2) === 2.5)
+    }
 }
