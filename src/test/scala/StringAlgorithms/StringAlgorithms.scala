ac7a2a164e1adec6068d3ce8df67abb0e0d1c76d
guoJohnny <401870532@qq.com>
2020-06-16 09:04:12 +0800

    add Longest Palindrome test

--- src/test/scala/StringAlgorithms/StringAlgorithms.scala
+++ src/test/scala/StringAlgorithms/StringAlgorithms.scala
@@ -6,4 +6,8 @@ class StringAlgorithmsSpec extends FlatSpec {
     "Longest Substring Without Repeating Characters" should "return a number passed to it" in {  
       assert(StringAlgorithms.lengthOfLongestSubstring("abcabcbb") === 3)
     }
+
+    "Longest Palindromic Substring" should "return a string passed to it" in {  
+      assert(StringAlgorithms.longestPalindrome("babad") === "bab")
+    }
 }
